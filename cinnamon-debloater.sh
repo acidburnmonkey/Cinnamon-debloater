fcfe5d1d6b00840cdf02a7e16685369ca73a0a41
acidburnmonkey <107169785+acidburnmonkey@users.noreply.github.com>
2022-10-30 23:32:00 -0400

    Add files via upload

---
+++ cinnamon-debloater.sh
@@ -0,0 +1,11 @@
+#Cinnamon desktop debloater
+
+#apps deleted 
+sudo dnf remove hexchat transmission hypnotix rhythmbox warpinator xfburn pidgin xawtv
+
+#delete mint default icons and themes
+sudo rm -r -v /usr/share/icons/Mint* /usr/share/icons/Ubuntu*
+
+#delete mint themes
+sudo rm -r -v /usr/share/themes/Mint*
+
