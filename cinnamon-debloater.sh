e044dac460ab1286c26f929792ea21927bdc37eb
acidburnmonkey <107169785+acidburnmonkey@users.noreply.github.com>
2022-11-15 04:19:53 -0500

    Update cinnamon-debloater.sh

--- cinnamon-debloater.sh
+++ cinnamon-debloater.sh
@@ -1,11 +1,17 @@
 #Cinnamon desktop debloater
 
 #apps deleted 
-sudo dnf remove hexchat transmission hypnotix rhythmbox warpinator xfburn pidgin xawtv redshift
+if command -v dnf &> /dev/null; then
+  sudo dnf -v remove hexchat transmission hypnotix rhythmbox warpinator xfburn pidgin xawtv   
+fi
+
+if  command -v apt &> /dev/null; then
+  sudo apt purge -v remove hexchat transmission hypnotix rhythmbox warpinator xfburn pidgin xawtv   
+fi
+
 
 #delete mint default icons and themes
-sudo rm -r -v /usr/share/icons/Mint* /usr/share/icons/Ubuntu*
-
+sudo rm -r  /usr/share/icons/Mint* /usr/share/icons/Ubuntu*
 #delete mint themes
-sudo rm -r -v /usr/share/themes/Mint*
+sudo rm -r /usr/share/themes/Mint*
 
